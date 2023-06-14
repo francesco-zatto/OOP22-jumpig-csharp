d9b0979538f3d06d440e487d85df8e4f34da060e
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-06-14 18:43:12 +0200

    Added NUnit package

--- FrancescoZattoni/Test/CollisionHandlerTest.cs
+++ FrancescoZattoni/Test/CollisionHandlerTest.cs
@@ -1,7 +1,19 @@
+using NUnit.Framework;
+
 namespace FrancescoZattoni.Test
 {
     class CollisionHandlerTest
     {
-        
+        [Test]
+        public void testBasicPlatformCollision()
+        {
+            Assert.AreEqual(0, 0);
+        }
+
+        [Test]
+        public void testPlayerIsNotGoingDown()
+        {
+            Assert.AreEqual(1, 1);
+        }
     }
 }
