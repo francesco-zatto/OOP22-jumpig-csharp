1ef9d2c91b7dbfabae8d665509d0f20842ca7b3c
Alessandro <alessandro.verna@studio.unibo.it>
2023-06-15 17:09:10 +0200

    Created test TestSetters

--- AlessandroVerna/Test/PlayerTest.cs
+++ AlessandroVerna/Test/PlayerTest.cs
@@ -1,1 +1,20 @@
 using NUnit.Framework;
+
+namespace AlessandroVerna.Test
+{
+    class PlayerTest
+    {
+        [Test]
+        public void TestSetters()
+        {
+            var player = new PlayerImpl(new PositionImpl(5,5));
+            Assert.IsTrue(player.Velocity.YComponent == 20);
+            player.Velocity = new VelocityImpl(0, 0);
+            Assert.IsTrue(player.Velocity.YComponent == 0);
+            Assert.AreEqual(1, 1);
+            Assert.IsFalse(player.PlatformHeight.HasValue);
+            player.PlatformHeight = 10;
+            Assert.IsTrue(player.PlatformHeight.HasValue);
+        } 
+    }
+}
