d36669a5044ca5107fe566855d7eb5eeb6664bc8
Francesco <francesco.filippini8@studio.unibo.it>
2023-06-15 22:51:28 +0200

    Added methods in Hitbox

--- FrancescoFilippini/Hitbox.cs
+++ FrancescoFilippini/Hitbox.cs
@@ -5,9 +5,22 @@ namespace FrancescoFilippini
 public interface Hitbox
 {
     /*
-    *The method to get the center of the shape.
-    @return the center of the shape.
+    * The method to get the center of the shape.
+    * @return the center of the shape.
     */
     PositionImpl GetCenter();
 
+    /*
+    * The method to update the HitBox position.
+    * @param center the abscissa and the ordinate of the center of the Hitbox.
+    */
+    void UpdateHitBox(PositionImpl center);
+
+    double getLeftX();
+
+    double getRightX();
+
+    double getUpperY();
+
+    double getLowerY();
 }
