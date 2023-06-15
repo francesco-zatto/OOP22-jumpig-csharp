4077a55843b5d3cba39c5e2286dedace8519d781
Alessandro <alessandro.verna@studio.unibo.it>
2023-06-15 19:38:27 +0200

    Created test TestTop10 and method CreateLeaderboard

--- AlessandroVerna/Test/LeaderboardTest.cs
+++ AlessandroVerna/Test/LeaderboardTest.cs
@@ -7,7 +7,8 @@ namespace AlessandroVerna.Test
         private static readonly int HEIGHT_SCORE = 100;
         private static readonly int COINS = 30;
         private static readonly int NUM_SCORES = 13;
-        private static Random RANDOM = new();
+        private static readonly Random RANDOM = new();
+        private static int TOP_10 = 10;
 
         [Test]
         public void TestAddLeaderboard()
@@ -26,6 +27,21 @@ namespace AlessandroVerna.Test
             Assert.AreNotEqual(copy, leaderboard);
             var copy2 = leaderboard;
             Assert.AreEqual(copy2, leaderboard);
+        }
+
+        [Test]
+        public void TestTop10()
+        {
+            var leaderboard = new LeaderboardImpl();
+            CreateLeaderboard(leaderboard);
+            Assert.AreEqual(TOP_10, leaderboard.Scores.Count);
+        }
+
+        private static void CreateLeaderboard(ILeaderboard leaderboard) {
+            for (int i = 0; i < NUM_SCORES; i++)
+            {
+                leaderboard.AddScore(new ScoreImpl("a"+i, RANDOM.Next(0, HEIGHT_SCORE), RANDOM.Next(0, COINS)));
+            }
         }
     }
 }
