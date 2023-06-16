f15da88b2431a56811b3744ed309fbd2a30c0c80
Alessandro <alessandro.verna@studio.unibo.it>
2023-06-16 09:17:41 +0200

    Created test TestLives

--- AlessandroVerna/Test/PlayerTest.cs
+++ AlessandroVerna/Test/PlayerTest.cs
@@ -7,7 +7,7 @@ namespace AlessandroVerna.Test
         [Test]
         public void TestSetters()
         {
-            var player = new PlayerImpl(new PositionImpl(5,5));
+            var player = new PlayerImpl(new PositionImpl(5, 5));
             Assert.IsTrue(player.Velocity.YComponent == 20);
             player.Velocity = new VelocityImpl(0, 0);
             Assert.IsTrue(player.Velocity.YComponent == 0);
@@ -15,6 +15,18 @@ namespace AlessandroVerna.Test
             Assert.IsTrue(player.PlatformHeight.HasValue);
             player.PlatformHeight = 10;
             Assert.IsTrue(player.PlatformHeight.HasValue);
+        }
+
+        [Test]
+        public void TestLives()
+        {
+            var player = new PlayerImpl(new PositionImpl(5, 5));
+            Assert.AreEqual(3, player.Lives);
+            player.DecreaseLives();
+            Assert.AreEqual(2, player.Lives);
+            player.DecreaseLives();
+            player.DecreaseLives();
+            Assert.AreEqual(0, player.Lives);
         } 
     }
 }
