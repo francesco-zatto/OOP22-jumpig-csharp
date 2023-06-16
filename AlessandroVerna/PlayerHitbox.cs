eabaed12a218530fe506d0a5cd85ccecf87230a9
Alessandro <alessandro.verna@studio.unibo.it>
2023-06-16 11:46:36 +0200

    Created class PlayerHitbox

---
+++ AlessandroVerna/PlayerHitbox.cs
@@ -0,0 +1,16 @@
+using FrancescoFilippini;
+
+namespace AlessandroVerna
+{
+    public class PlayerHitbox : RectangleHitbox
+    {
+        
+        private static readonly double PLAYER_WIDTH = 6;
+        private static readonly double PLAYER_HEIGHT = 6;
+
+        public PlayerHitbox(PositionImpl center)
+            : base(center, PLAYER_WIDTH, PLAYER_HEIGHT)
+        {
+        }
+    }
+}
