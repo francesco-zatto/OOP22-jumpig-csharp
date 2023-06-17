22daaf53a6b47f44745c094683e9224cac73927c
Francesco <francesco.filippini8@studio.unibo.it>
2023-06-17 18:50:43 +0200

    Implemented BasicCoin

---
+++ FrancescoFilippini/BasicCoin.cs
@@ -0,0 +1,27 @@
+using System;
+using FrancescoZattoni;
+using AlessandroVerna;
+
+namespace FrancescoFilippini
+{
+    public class BasicCoin : AbstractGameEntity<CircleHitbox>, ICoin
+    {
+        private bool taken;
+        private readonly ICollisionHandler<CircleHitbox, ICoin> collisionHandler = 
+            new CollisionHandlerImpl<CircleHitbox, ICoin>(new CoinCollisionActioner(), 
+            new CoinCollisionChecker());
+
+        public override bool IsTaken => taken;
+
+        public BasicCoin(IPosition position) : base(position, new CoinHitbox(position))
+        {
+        }
+
+        public override void MarkTarget() => taken = true;
+
+        public override void HandleCollision(IPlayer player) => collisionHandler.Handle(player, this);
+
+        public override CircleHitbox CreateScaledHitbox(IPosition position) => new CoinHitbox(position);
+
+    }
+}
