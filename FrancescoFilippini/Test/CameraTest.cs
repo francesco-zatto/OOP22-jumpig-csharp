18f9ecc82651ada16230acb089201a629e404edf
Francesco <francesco.filippini8@studio.unibo.it>
2023-06-19 22:54:13 +0200

    Implemented method TestCameraMovement

--- FrancescoFilippini/Test/CameraTest.cs
+++ FrancescoFilippini/Test/CameraTest.cs
@@ -7,7 +7,7 @@ namespace FrancescoFilippini.Test
     {
         private static readonly double WIDTH = 16;
         
-        private static readonly double GRAVITY = 10;
+        private static readonly double GRAVITY = -10;
 
         [Test]
         public static void TestSameVelocity()
@@ -25,6 +25,18 @@ namespace FrancescoFilippini.Test
         camera.SetCameraHeight(time, player2);
         player2.ComputePosition(time);
         Assert.AreEqual(player2.Position, camera.CameraHeight);
+        }
+
+        [Test]
+        public static void TestCameraMovement()
+        {
+        IPlayer player = new PlayerImpl(new PositionImpl(WIDTH / 2, 0));
+        ICamera camera = new CameraImpl(player);
+        var time = 1;
+        player.ComputeVelocity(GRAVITY, time, Direction.HorizontalZero);
+        camera.CameraHeight = 0;
+        camera.SetCameraHeight(time, player); // velocity of the camera should be 0 when player goes down
+        Assert.AreEqual(camera.CameraHeight, 0);
         }
 
 
